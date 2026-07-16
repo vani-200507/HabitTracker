3cc24eff3f1f33c7ea5fefee67b2361c1384c07d
[vani-200507] <[s160423119@student.ubaya.ac.id]>
2026-07-16 11:54:17 +0700

    Refactor AddHabitFragment using Data Binding

--- app/src/main/java/com/example/habittracker/view/AddHabitFragment.kt
+++ app/src/main/java/com/example/habittracker/view/AddHabitFragment.kt
@@ -1,60 +1,68 @@
 package com.example.habittracker.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.habittracker.R
+import com.example.habittracker.databinding.FragmentAddHabitBinding
 import com.example.habittracker.model.Habit
 import com.example.habittracker.viewmodel.HabitViewModel
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
 
-class AddHabitFragment : Fragment(R.layout.fragment_add_habit) {
+class AddHabitFragment : Fragment() {
+
+    private var _binding: FragmentAddHabitBinding? = null
+    private val binding get() = _binding!!
 
     private val viewModel: HabitViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentAddHabitBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
-        val etName = view.findViewById<TextInputEditText>(R.id.etHabitName)
-        val etDesc = view.findViewById<TextInputEditText>(R.id.etDescription)
-        val etGoal = view.findViewById<TextInputEditText>(R.id.etGoal)
-        val etUnit = view.findViewById<TextInputEditText>(R.id.etUnit)
-        val dropdownIcon = view.findViewById<AutoCompleteTextView>(R.id.actvSelectIcon)
-        val btnSave = view.findViewById<MaterialButton>(R.id.btnCreateHabit)
-
-        // tombol back toolbar
-        toolbar.setNavigationOnClickListener {
+        binding.toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
 
-        setupDropdown(dropdownIcon)
+        setupDropdown()
 
-        btnSave.setOnClickListener {
+        binding.btnCreateHabit.setOnClickListener {
 
-            val name = etName.text.toString().trim()
-            val desc = etDesc.text.toString().trim()
-            val goalText = etGoal.text.toString().trim()
-            val unit = etUnit.text.toString().trim()
-            val iconName = dropdownIcon.text.toString()
+            val name = binding.etHabitName.text.toString().trim()
+            val desc = binding.etDescription.text.toString().trim()
+            val goalText = binding.etGoal.text.toString().trim()
+            val unit = binding.etUnit.text.toString().trim()
+            val iconName = binding.actvSelectIcon.text.toString()
 
-            if (name.isEmpty() || desc.isEmpty() || goalText.isEmpty() || unit.isEmpty()) {
+            if (
+                name.isEmpty() ||
+                desc.isEmpty() ||
+                goalText.isEmpty() ||
+                unit.isEmpty()
+            ) {
+
                 Toast.makeText(
                     requireContext(),
                     "Semua data harus diisi!",
                     Toast.LENGTH_SHORT
                 ).show()
+
                 return@setOnClickListener
             }
-
-            val iconRes = getIcon(iconName)
 
             val habit = Habit(
                 name = name,
@@ -62,7 +70,7 @@ class AddHabitFragment : Fragment(R.layout.fragment_add_habit) {
                 goal = goalText.toInt(),
                 progress = 0,
                 unit = unit,
-                icon = iconRes
+                icon = getIcon(iconName)
             )
 
             viewModel.insertHabit(habit)
@@ -77,8 +85,9 @@ class AddHabitFragment : Fragment(R.layout.fragment_add_habit) {
         }
     }
 
-    private fun setupDropdown(dropdown: AutoCompleteTextView) {
-        val options = listOf(
+    private fun setupDropdown() {
+
+        val listIcon = listOf(
             "Belajar",
             "Minum Air",
             "Makan Sehat",
@@ -90,22 +99,35 @@ class AddHabitFragment : Fragment(R.layout.fragment_add_habit) {
         val adapter = ArrayAdapter(
             requireContext(),
             android.R.layout.simple_dropdown_item_1line,
-            options
+            listIcon
         )
 
-        dropdown.setAdapter(adapter)
-        dropdown.setText(options[0], false)
+        binding.actvSelectIcon.setAdapter(adapter)
+        binding.actvSelectIcon.setText(listIcon[0], false)
     }
 
-    private fun getIcon(name: String): Int {
-        return when (name) {
+    private fun getIcon(icon: String): Int {
+
+        return when (icon) {
+
             "Belajar" -> R.drawable.ic_study
+
             "Minum Air" -> R.drawable.ic_water
+
             "Makan Sehat" -> R.drawable.ic_food
+
             "Membaca" -> R.drawable.ic_reading
+
             "Tidur" -> R.drawable.ic_sleep
+
             "Relaksasi" -> R.drawable.ic_meditation
+
             else -> R.drawable.ic_study
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
