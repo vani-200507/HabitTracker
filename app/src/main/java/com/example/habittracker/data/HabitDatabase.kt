66980ff45ad87560e568c651c5b2f190ff03aee1
[vani-200507] <[s160423119@student.ubaya.ac.id]>
2026-07-16 17:08:03 +0700

    Add User Room database and default account

--- app/src/main/java/com/example/habittracker/data/HabitDatabase.kt
+++ app/src/main/java/com/example/habittracker/data/HabitDatabase.kt
@@ -4,16 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.habittracker.model.Habit
+import com.example.habittracker.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(
-    entities = [Habit::class],
-    version = 1,
+    entities = [
+        Habit::class,
+        User::class
+    ],
+    version = 2,
     exportSchema = false
 )
 abstract class HabitDatabase : RoomDatabase() {
 
     abstract fun habitDao(): HabitDao
+
+    abstract fun userDao(): UserDao
 
     companion object {
 
@@ -28,7 +38,27 @@ abstract class HabitDatabase : RoomDatabase() {
                     context.applicationContext,
                     HabitDatabase::class.java,
                     "habit_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .addCallback(object : Callback() {
+
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+
+                            CoroutineScope(Dispatchers.IO).launch {
+
+                                INSTANCE?.userDao()?.insertUser(
+                                    User(
+                                        username = "student",
+                                        password = "123"
+                                    )
+                                )
+
+                            }
+                        }
+
+                    })
+                    .build()
 
                 INSTANCE = instance
                 instance
