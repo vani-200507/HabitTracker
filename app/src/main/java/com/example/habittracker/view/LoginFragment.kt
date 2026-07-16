749512d39680b7364a4fe557cac2d44d125a6576
[vani-200507] <[s160423119@student.ubaya.ac.id]>
2026-07-16 17:08:54 +0700

    Implement Room login and login session

--- app/src/main/java/com/example/habittracker/view/LoginFragment.kt
+++ app/src/main/java/com/example/habittracker/view/LoginFragment.kt
@@ -7,14 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.habittracker.R
+import com.example.habittracker.data.HabitDatabase
 import com.example.habittracker.databinding.FragmentLoginBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var db: HabitDatabase
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +36,21 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        db = HabitDatabase.buildDatabase(requireContext())
+
+        // BONUS: Auto Login
+        val pref = requireActivity().getSharedPreferences(
+            "habit_pref",
+            Context.MODE_PRIVATE
+        )
+
+        val isLogin = pref.getBoolean("is_login", false)
+
+        if (isLogin) {
+            findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+            return
+        }
+
         binding.btnLogin.setOnClickListener {
 
             val username = binding.editUsername.text.toString().trim()
@@ -42,28 +64,40 @@ class LoginFragment : Fragment() {
                     Toast.LENGTH_SHORT
                 ).show()
 
-            } else if (username == "student" && password == "123") {
+                return@setOnClickListener
+            }
 
-                val pref = requireActivity()
-                    .getSharedPreferences("habit_pref", Context.MODE_PRIVATE)
+            lifecycleScope.launch {
 
-                pref.edit()
-                    .putString("username", username)
-                    .apply()
+                val user = withContext(Dispatchers.IO) {
+                    db.userDao().login(username, password)
+                }
 
-                findNavController()
-                    .navigate(R.id.action_loginFragment_to_dashboardFragment)
+                if (user != null) {
 
-            } else {
+                    pref.edit()
+                        .putBoolean("is_login", true)
+                        .putString("username", user.username)
+                        .apply()
 
-                Toast.makeText(
-                    requireContext(),
-                    "Username atau Password salah!",
-                    Toast.LENGTH_SHORT
-                ).show()
+                    findNavController().navigate(
+                        R.id.action_loginFragment_to_dashboardFragment
+                    )
+
+                } else {
+
+                    Toast.makeText(
+                        requireContext(),
+                        "Username atau Password salah!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
 
             }
+
         }
+
     }
 
     override fun onDestroyView() {
